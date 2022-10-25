2f0842c6bd63a70fe622e94e82c13cf809e2b134
Haizea <haize@DESKTOP-8QPK9LI>
2022-10-25 08:53:42 +0200

    New: ModelFactory (finsihed) & ModelImplementation

---
+++ src/model/ModelFactory.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import datatransferobject.Model;
+
+/**
+ *
+ * @author haize
+ */
+public class ModelFactory {
+        private static Model model;
+    
+    public static Model getModel() {
+        if (model == null) {
+            model = new ModelImplementation();
+        }
+        return model;
+    }
+}
---
+++ src/model/ModelImplementation.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import datatransferobject.Model;
+import datatransferobject.Package;
+import datatransferobject.User;
+
+/**
+ *
+ * @author haize
+ */
+public class ModelImplementation implements Model {
+
+    @Override
+    public Package doSignIn(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Package doSignUp(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
