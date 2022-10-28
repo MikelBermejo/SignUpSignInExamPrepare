8fec2688515a3b4eeaa0c5554b06127881c5028f
MikelBermejo <mikelbermejofdez@gmail.com>
2022-10-28 12:34:55 +0200

    all focuslost done

--- src/view/SignUpVController.java
+++ src/view/SignUpVController.java
@@ -6,6 +6,7 @@
 package view;
 
 
+import java.util.function.UnaryOperator;
 import javafx.event.ActionEvent;
 import javafx.event.EventType;
 import javafx.scene.input.KeyEvent;
@@ -22,6 +23,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import java.util.logging.Logger;
+import javafx.beans.Observable;
 
 /**
  *
@@ -105,18 +108,23 @@ public class SignUpVController{
         stage.setResizable(false);
         //Add Listeners
         //Writing, key typed
-        textFieldUsername.textProperty().addListener((event) -> this.textChanged(KeyEvent.KEY_TYPED));
-        textFieldPassword.textProperty().addListener((event) -> this.textChanged(KeyEvent.KEY_TYPED, KeyEvent.KEY_PRESSED));
-        passwordField.textProperty().addListener((event) -> this.textChanged(KeyEvent.KEY_TYPED, KeyEvent.KEY_PRESSED));
-        textFieldPassword.textProperty().addListener((event) -> this.textChanged(KeyEvent.KEY_PRESSED));
-        passwordField.textProperty().addListener((event) -> this.textChanged(KeyEvent.KEY_PRESSED));
-        textFieldEmail.textProperty().addListener((event) -> this.textChanged(KeyEvent.KEY_TYPED));
-        textFieldName.textProperty().addListener((event) -> this.textChanged(KeyEvent.KEY_TYPED));
+        textFieldUsername.setOnKeyTyped(this::textChanged);
+        textFieldPassword.setOnKeyTyped(this::textChanged);
+        passwordField.setOnKeyTyped(this::textChanged);
+        passwordField.setOnKeyReleased(this::textChangedPressed);
+        textFieldPassword.setOnKeyReleased(this::textChangedPressed);
+        textFieldEmail.setOnKeyTyped(this::textChanged);
+        textFieldName.setOnKeyTyped(this::textChanged);
         //
         //Focus lost
-        passwordField.focusedProperty().addListener((event) -> this.focusedPropertyChanged());
-        textFieldUsername.focusedProperty().addListener((event) -> this.focusedPropertyChanged());
-        textFieldPassword.focusedProperty().addListener((event) -> this.focusedPropertyChanged());
+        textFieldPassword.focusedProperty().addListener(this::focusedPropertyChangedPassword);
+        passwordField.focusedProperty().addListener(this::focusedPropertyChangedPassword);
+        textFieldEmail.focusedProperty().addListener(this::focusedPropertyChangedEmail);
+        textFieldUsername.focusedProperty().addListener(this::focusedPropertyChanged);
+        textFieldName.focusedProperty().addListener(this::focusedPropertyChangedName);
+        textFieldConfirmPassword.focusedProperty().addListener(this::focusedPropertyChangedPasswordConfirm);
+        passwordFieldConfirm.focusedProperty().addListener(this::focusedPropertyChangedPasswordConfirm);
+        
         //
         //Button Actions
         buttonSignIn.pressedProperty().addListener((event) -> this.signIn(ActionEvent.ACTION));
@@ -136,47 +144,105 @@ public class SignUpVController{
         // Si no devuelve ninguna excepción abre la ventana SignIn y cierra la actual.
         // Si devuelve una excepción se muestra una ventana emergente que muestra el error.
 
-    private void textChanged(EventType<KeyEvent> KEY_TYPED) {
-        if(textFieldUsername.getText().length()>25){
+    private void textChanged(KeyEvent event) {
+        if(textFieldUsername.getText().length()>=25){
+            event.consume();
             textFieldUsername.setText(textFieldUsername.getText().substring(0,25));
         }
-        if(textFieldPassword.getText().length()>25){
+        if(textFieldPassword.getText().length()>=25){
+            event.consume();
             textFieldPassword.setText(textFieldPassword.getText().substring(0,25));
         }
-        if(passwordField.getText().length()>25){
+        if(passwordField.getText().length()>=25){
+            event.consume();
             passwordField.setText(passwordField.getText().substring(0,25));
         }
-        if(textFieldEmail.getText().length()>35){
+        if(textFieldEmail.getText().length()>=35){
+            event.consume();
             textFieldEmail.setText(textFieldEmail.getText().substring(0,35));
         }
-        if(passwordField.getText().length()>50){
+        if(textFieldName.getText().length()>=50){
+            event.consume();
             textFieldName.setText(textFieldName.getText().substring(0,50));
         }
     }
     
-    private void focusedPropertyChanged() {
-        if(!passwordField.isFocused() && !textFieldPassword.isFocused()){
-            if(passwordField.getText().contains(" ")){
-                imageViewPassword.setImage(new Image(getClass().getResourceAsStream("/resources/iconPasswordRedIncorrect.png")));
-                linePassword.setStroke(Color.RED);
-                labelInvalidPassword.setText("Password can't contain an empty space.");
-                labelInvalidPassword.setVisible(true);
-            } else {
-                imageViewPassword.setImage(new Image(getClass().getResourceAsStream("/resources/iconPassword.png")));
-                linePassword.setStroke(Color.GREY);
-                labelInvalidPassword.setVisible(false);
+    private void focusedPropertyChangedName(Observable value, Boolean oldValue, Boolean newValue) {
+        if(oldValue){
+            if(!textFieldName.isFocused()){
+                if(textFieldName.getText().isEmpty()){
+                    imageViewName.setImage(new Image(getClass().getResourceAsStream("/resources/iconFullNameIncorrect.png")));
+                    lineName.setStroke(Color.RED);
+                    labelInvalidName.setText("Full name is empty.");
+                } else {
+                    imageViewName.setImage(new Image(getClass().getResourceAsStream("/resources/iconFullName.png")));
+                    lineName.setStroke(Color.GREY);
+                    labelInvalidName.setText("");
+                }
             }
         }
-        if(!textFieldUsername.isFocused()){
-            if(textFieldUsername.getText().contains(" ")){
-                imageViewUsername.setImage(new Image(getClass().getResourceAsStream("/resources/iconUserInconrrect.png")));
-                lineUsername.setStroke(Color.RED);
-                labelInvalidUser.setText("Username can't contain an empty space.");
-                labelInvalidUser.setVisible(true);
-            } else {
-                imageViewUsername.setImage(new Image(getClass().getResourceAsStream("/resources/iconUser.png")));
-                lineUsername.setStroke(Color.GREY);
-                labelInvalidUser.setVisible(false);
+    }
+    
+    private void focusedPropertyChangedEmail(Observable value, Boolean oldValue, Boolean newValue) {
+        if(oldValue){  
+            if(!textFieldEmail.isFocused()){    
+                if(textFieldEmail.getText().matches("^[A-Za-z0-9+_.-]+@(.+)[A-Za-z0-9+_.-]")) {
+                    imageViewEmail.setImage(new Image(getClass().getResourceAsStream("/resources/iconEmail.png")));
+                    lineEmail.setStroke(Color.GREY);
+                    labelInvalidEmail.setText("");
+                } else {
+                    imageViewEmail.setImage(new Image(getClass().getResourceAsStream("/resources/iconEmailIncorrect.png")));
+                    lineEmail.setStroke(Color.RED);
+                    labelInvalidEmail.setText("Invalid format of email (*@*.*)");
+                }
+                
+            }
+        }
+    }
+    private void focusedPropertyChanged(Observable value, Boolean oldValue, Boolean newValue) {
+        if(oldValue){
+            if(!textFieldUsername.isFocused()){
+                if(textFieldUsername.getText().contains(" ") || textFieldUsername.getText().isEmpty()){
+                    imageViewUsername.setImage(new Image(getClass().getResourceAsStream("/resources/iconUserInconrrect.png")));
+                    lineUsername.setStroke(Color.RED);
+                    labelInvalidUser.setText("Username can't be empty nor contain an empty space.");
+                } else {
+                    imageViewUsername.setImage(new Image(getClass().getResourceAsStream("/resources/iconUser.png")));
+                    lineUsername.setStroke(Color.GREY);
+                    labelInvalidUser.setText("");
+                }
+            }
+        }
+    }
+    
+     private void focusedPropertyChangedPasswordConfirm(Observable value, Boolean oldValue, Boolean newValue){
+        if(oldValue){
+            if(!passwordFieldConfirm.isFocused() && !textFieldConfirmPassword.isFocused()){
+                if(!passwordFieldConfirm.getText().toString().equalsIgnoreCase(passwordField.getText().toString())){
+                    imageViewConfirmPassword.setImage(new Image(getClass().getResourceAsStream("/resources/iconPasswordRedIncorrect.png")));
+                    lineConfirmPassword.setStroke(Color.RED);
+                    labelInvalidConfirmPassword.setText("Doesn't match with the password.");
+                } else {
+                    imageViewConfirmPassword.setImage(new Image(getClass().getResourceAsStream("/resources/iconPassword.png")));
+                    lineConfirmPassword.setStroke(Color.GREY);
+                    labelInvalidConfirmPassword.setText("");
+                }
+            }
+        }
+    }
+    
+    private void focusedPropertyChangedPassword(Observable value, Boolean oldValue, Boolean newValue){
+        if(oldValue){
+            if(!passwordField.isFocused() && !textFieldPassword.isFocused()){
+                if(passwordField.getText().contains(" ") || passwordField.getText().length()<8 || passwordField.getText().isEmpty()){
+                    imageViewPassword.setImage(new Image(getClass().getResourceAsStream("/resources/iconPasswordRedIncorrect.png")));
+                    linePassword.setStroke(Color.RED);
+                    labelInvalidPassword.setText("Password can't be empty nor contain an empty space or his lenght is less than 8.");
+                } else {
+                    imageViewPassword.setImage(new Image(getClass().getResourceAsStream("/resources/iconPassword.png")));
+                    linePassword.setStroke(Color.GREY);
+                    labelInvalidUser.setText("");
+                }
             }
         }
     }
@@ -186,7 +252,11 @@ public class SignUpVController{
     }
 
     private void signUp(EventType<ActionEvent> ACTION) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        focusedPropertyChangedPassword(null, true, false);
+        focusedPropertyChangedPasswordConfirm(null, true, false);
+        focusedPropertyChanged(null, true, false);
+        focusedPropertyChangedEmail(null, true, false);
+        focusedPropertyChangedName(null, true, false);
     }
 
     private void ShowHide(EventType<ActionEvent> ACTION) {
@@ -210,8 +280,12 @@ public class SignUpVController{
         }
     }
 
-    private void textChanged(EventType<KeyEvent> KEY_TYPED, EventType<KeyEvent> KEY_PRESSED) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void textChangedPressed(KeyEvent KEY_RELEASED) {
+        if (passwordField.isVisible()){
+            textFieldPassword.setText(passwordField.getText());
+        } else if (textFieldPassword.isVisible()){
+            passwordField.setText(textFieldPassword.getText());
+        }
     }
 
     
